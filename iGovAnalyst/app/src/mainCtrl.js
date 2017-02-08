9ddd768b5a1b5c38fed5196b5bd95a6ddc26f11f
Valery Stavitsky <valery.stavitsky@gmail.com>
2017-02-08 18:21:29 +0200

    Update mainCtrl.js
    fix 21

--- iGovAnalyst/app/src/mainCtrl.js
+++ iGovAnalyst/app/src/mainCtrl.js
@@ -2,12 +2,12 @@
 
 
 angular.module("App")
-    .controller("mainCtrl", function ($locationProvider, $scope) {
+    .controller("mainCtrl", function ($scope) {
         
-       // $locationProvider.html5Mode(true);
+        //$locationProvider.html5Mode(true);
 
         //$scope.topMenu = "/app/view/topMenu.html";
         //$scope.mainMenu = "/app/view/mainMenu.html";
-        $scope.headMenu = "/app/view/headMenu.html";
+        $scope.headMenu = "app/view/headMenu.html";
 
     });
